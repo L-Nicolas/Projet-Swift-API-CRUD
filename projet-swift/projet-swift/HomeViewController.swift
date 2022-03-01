867bfda57c31713e5c16251bc699b62412b4679b
L-Nicolas <nicolas.lacoste.sio@gmail.com>
2022-03-01 16:45:10 +0100

    Liste rapport non scrollable

--- projet-swift/projet-swift/HomeViewController.swift
+++ projet-swift/projet-swift/HomeViewController.swift
@@ -7,15 +7,25 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UIScrollViewDelegate {
     
     @IBOutlet weak var homePresentationLabel: UILabel!
-    @IBOutlet var navBar: [UITabBar]!
-    
+    @IBOutlet weak var scrollView: UIScrollView!
     
     @IBOutlet weak var btnMesRapports: UIButton!
     @IBOutlet weak var newRapport: UIButton!
+    @IBOutlet weak var rapportTableView: UITableView!
+    
+    let screenHeight = UIScreen.main.bounds.height
+    let scrollViewContentHeight = 1200 as CGFloat
+    
+    static let albumCellID = "_ALBUM_CELL_ID"
     var token: String = ""
+    var rapportsTable: [Rapport] = [] {
+        didSet {
+            self.rapportTableView.reloadData()
+        }
+    }
     
     public class func newInstance(token: String) -> HomeViewController {
         let hvc = HomeViewController()
@@ -25,11 +35,45 @@ class HomeViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+            scrollView.delegate = self
+        rapportTableView.delegate = self
+            scrollView.bounces = false
+        rapportTableView.bounces = false
+        rapportTableView.isScrollEnabled = false
+        self.rapportTableView.dataSource = self
+        
         self.navigationItem.setHidesBackButton(true, animated: true);
         self.homePresentationLabel.text = "BIENVENUE DANS VOTRE ESPACE LOCATAIRE"
         self.homePresentationLabel.font = UIFont.boldSystemFont(ofSize: 30)
         self.homePresentationLabel.textAlignment = NSTextAlignment.left;
         self.homePresentationLabel.numberOfLines = 4
+        
+        if self.token.isEmpty {
+            let alert = UIAlertController(title: "Problème", message: "Impossible de récuprer vos rapports", preferredStyle: UIAlertController.Style.alert)
+            alert.addAction(UIAlertAction(title: "Recommencer", style: UIAlertAction.Style.default, handler: nil))
+            self.present(alert, animated: true, completion: nil)
+            return
+        } else {
+            
+            Task {
+                let result = await ListRapportWebService.getListRapportImm(token: self.token)
+                dump(result)
+                print("immeuuuuble")
+                
+                if result["error"] as! Int == 1 {
+                    let alert = UIAlertController(title: "Problème", message: result["message"] as? String, preferredStyle: UIAlertController.Style.alert)
+                    alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+                    self.present(alert, animated: true, completion: nil)
+                } else {
+                    guard let resultData = result["data"] as? [Rapport] else {
+                        return
+                    }
+                    dump(resultData[0])
+                    self.rapportsTable = resultData
+                    //self.registerTableViewCells()
+                }
+            }
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -47,5 +91,37 @@ class HomeViewController: UIViewController {
     @IBAction func newRapport(_ sender: Any) {
         let nextController = CreateRapportViewController.newInstance(token: self.token)
         self.navigationController?.pushViewController(nextController, animated: true)
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return self.rapportsTable.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: HomeViewController.albumCellID) ?? UITableViewCell(style: .default, reuseIdentifier: HomeViewController.albumCellID)
+        let rapport = self.rapportsTable[indexPath.row]
+        print("affichage")
+        dump(rapportsTable)
+        cell.textLabel?.text = "Rapport : \(rapport.id)"
+        return cell
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let yOffset = scrollView.contentOffset.y
+
+        if scrollView == self.scrollView {
+            if yOffset >= scrollViewContentHeight - screenHeight {
+                scrollView.isScrollEnabled = false
+                rapportTableView.isScrollEnabled = true
+            }
+        }
+
+        if scrollView == self.rapportTableView {
+            if yOffset <= 0 {
+                self.scrollView.isScrollEnabled = true
+                self.rapportTableView.isScrollEnabled = false
+            }
+        }
     }
 }
--- projet-swift/projet-swift/ListRapportWebService.swift
+++ projet-swift/projet-swift/ListRapportWebService.swift
@@ -40,4 +40,37 @@ class ListRapportWebService {
             return dic
         }
     }
+    
+    static func getListRapportImm(token: String) async -> [String: AnyObject]  {
+        
+        var dic:[String:AnyObject] = [:]
+        dic["error"] = 1 as AnyObject
+        dic["message"] = "Impossible de récupérer les rapports" as AnyObject
+        
+        var request = URLRequest(url: URL(string: "http://localhost:3000/api/user/rapport/liste/immeuble/")!)
+        let parameters = "token=" + token
+        
+        dump(parameters)
+
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpBody = parameters.data(using: String.Encoding.utf8)
+        
+        
+        do {
+            let (data, _) = try await URLSession.shared.data(for: request)
+            let json = try JSONSerialization.jsonObject(with: data) as! Dictionary<String, AnyObject>
+            dic["error"] = json["error"] as! NSNumber
+            dic["message"] = json["message"] as AnyObject
+            dic["data"] = json["data"] as AnyObject
+            
+            //let resultData = try JSONSerialization.jsonObject(with: json["data"])
+            let listRapports = RapportFactory.rapports(from: json["data"] as! [[String : AnyObject]])
+            dic["data"] = listRapports as AnyObject
+            return dic
+        }
+        catch {
+            return dic
+        }
+    }
 }
